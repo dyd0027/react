91dbe4a0d86a5ad4d3f87ac6781b191c0b7893b2
triple <dyd002700@gmail.com>
2023-04-26 21:47:00 +0900

    메인 화면 디자인

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,9 +1,64 @@
 import './App.css';
+import { Button,Navbar,Container,Nav,NavDropdown } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <button type="button" class="btn btn-primary">Primary</button>
+      <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Yong Shop</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <Jumbotron></Jumbotron>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-4'>
+          <img src='https://codingapple1.github.io/shop/shoes1.jpg' width="100%"/>
+          <h4>상품명</h4>
+          <p>상품설명 & 가격</p>
+        </div>
+        <div className='col-md-4'>
+          <img src='https://codingapple1.github.io/shop/shoes2.jpg' width="100%"/>
+          <h4>상품명</h4>
+          <p>상품설명 & 가격</p>
+        </div>
+        <div className='col-md-4'>
+          <img src='https://codingapple1.github.io/shop/shoes3.jpg' width="100%"/>
+          <h4>상품명</h4>
+          <p>상품설명 & 가격</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+}
+
+function Jumbotron(){
+  return(
+    <div className='background'>
+      <h2>20$ Season Off</h2>
+      <p>권용휘 짱짱맨</p>
+      <p>
+      <Button variant="primary">Primary</Button>{' '}
+      </p>
     </div>
   );
 }
