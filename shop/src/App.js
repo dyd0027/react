18943cf1d35fe562ba252e2cc825633b739bc0d9
triple <dyd002700@gmail.com>
2023-04-29 19:13:31 +0900

    라우터

--- shop/src/App.js
+++ shop/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { Button,Navbar,Container,Nav,NavDropdown } from 'react-bootstrap';
 import data from './data.js';
 
+import {Link, Route, Switch} from 'react-router-dom';
+
 function App() {
 
   let [shoes,shoes변경] = useState(data);
@@ -32,16 +34,33 @@ function App() {
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    <Jumbotron></Jumbotron>
-    <div className='container'>
-      <div className='row'>
-        {
-          shoes.map((a,i)=>{
-             return <Card shoes={shoes[i]} i={i} key={i}/>
-          })
-        }        
+    <Route exact path="/">
+    <Jumbotron/> 
+      <div className='container'>
+        <div className='row'>
+          {
+            shoes.map((a,i)=>{
+              return <Card shoes={shoes[i]} i={i} key={i}/>
+            })
+          }        
+        </div>
       </div>
-    </div>
+    </Route>
+    <Route path="/detail">
+      <div className='container'>
+          <div className='row'>
+            <div className='col-md-6'>
+              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%"></img>
+            </div>
+            <div className='col-md-6 mt-4'>
+              <h4 className='pt-5'>상품명</h4>
+              <p>상품 설명</p>
+              <p>120,000원</p>
+              <button className='btn btn-danger'>주문하기</button>  
+            </div>
+          </div>
+      </div>
+    </Route>
     </div>
   );
 }
