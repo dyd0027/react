ca2bd0e65aefac2f9088f96497c28251a12bc169
triple <dyd002700@gmail.com>
2023-05-09 20:49:01 +0900

    라우터2

--- shop/src/App.js
+++ shop/src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import './App.css';
 import { Button,Navbar,Container,Nav,NavDropdown } from 'react-bootstrap';
 import data from './data.js';
+import Detail from './Detail.js';
 
 import {Link, Route, Switch} from 'react-router-dom';
 
@@ -17,8 +18,8 @@ function App() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link><Link to="/">Home</Link></Nav.Link>
+            <Nav.Link><Link to="/detail">Detail</Link></Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
@@ -34,6 +35,8 @@ function App() {
         </Navbar.Collapse>
       </Container>
     </Navbar>
+
+    <Switch>
     <Route exact path="/">
     <Jumbotron/> 
       <div className='container'>
@@ -47,23 +50,18 @@ function App() {
       </div>
     </Route>
     <Route path="/detail">
-      <div className='container'>
-          <div className='row'>
-            <div className='col-md-6'>
-              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%"></img>
-            </div>
-            <div className='col-md-6 mt-4'>
-              <h4 className='pt-5'>상품명</h4>
-              <p>상품 설명</p>
-              <p>120,000원</p>
-              <button className='btn btn-danger'>주문하기</button>  
-            </div>
-          </div>
-      </div>
+      <Detail/>
     </Route>
+
+    <Route path="/:id">
+          <div>아무거나</div>
+    </Route>
+    </Switch>
     </div>
   );
 }
+
+
 
 function Jumbotron(){
   return(
