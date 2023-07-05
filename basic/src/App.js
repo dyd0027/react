0176cccacd7bf44955e59d03cade21158a3df9d1
GwonYongHwi <dyd002700@gmail.com>
2023-07-05 16:41:18 +0900

    4.5JSX문법 정리(심화)

--- basic/src/App.js
+++ basic/src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 
 function App() {
   const name ='용휘';
+  const list = ['가가','나나','다다'];
   return (
     <>
-      <h1 className='orange'>Hello!</h1>
+      <h1 className='orange'>{`Hello! ${name}`}</h1>
       <h2>Hellos</h2>
       <ul>
-        <li>가가</li>
-        <li>나니</li>
-        <li>{name}</li>
+        {
+          list.map((item) => (
+            <li>{item}</li>
+          ))
+        }
       </ul>
       <img style={{width: '300px', height:'100px;'}}src='https://sangsanginib.com/_upload//attFile/SS0002/SS0002_5_1.png' alt='ddd' />
       
