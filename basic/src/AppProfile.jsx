cf267780165f2d9e76d303ef3bb3f27ba02fa07c
GwonYongHwi <dyd002700@gmail.com>
2023-07-11 15:36:40 +0900

    4.11새로운 개발자 태그 만들기2

--- basic/src/AppProfile.jsx
+++ basic/src/AppProfile.jsx
@@ -9,7 +9,8 @@ function AppProfile() {
      <Profile 
       url = "https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
       name = "ㄱㅇㅎ"
-      title = "개발자"/>
+      title = "개발자"
+      isNew = {true}/>
       <Profile 
       url = "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80"
       name = "ㄱㅇㅎ2"
