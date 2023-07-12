e9dfda8585b32b7efb9f87571c846bb30f202f9b
GwonYongHwi <dyd002700@gmail.com>
2023-07-12 16:19:25 +0900

    4.18카운터 상태 끌어 올리기(props에 함수전달)

--- basic/src/component/Counter.jsx
+++ basic/src/component/Counter.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
 
-export default function Counter() {
+export default function Counter(props) {
   const [count, setCount] = useState(0);
   return (
     <div className='counter'>
-      <span className='number'>{count}</span>
+      <p className='number'>{count}<span className='total'>/{props.total}</span></p>
       <button
         className='button'
         onClick={() => {
           setCount((prev) => prev + 1);
+          props.onClick();
         }}
       >
         Add +
