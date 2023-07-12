755414e8a6466bcaa22e1eff560fa46cbcba4135
GwonYongHwi <dyd002700@gmail.com>
2023-07-12 16:58:25 +0900

    4.20useEffect제대로 사용하기 (클릭시 동적인  데이터 다르게 받아오기)

--- basic/src/component/Products.jsx
+++ basic/src/component/Products.jsx
@@ -3,9 +3,11 @@ import React, { useEffect, useState } from 'react';
 export default function Products() {
   const [count, setCount] = useState(0);
   const [products, setProducts] = useState([]);
+  const [checked,setChecked] = useState(false);
+  const handleChange = ()=> setChecked((prev)=>!prev);
 
   useEffect(() => {
-    fetch('data/products.json')
+    fetch(`data/${checked ? 'sale_':''}products.json`)
       .then((res) => res.json())
       .then((data) => {
         console.log('🔥뜨끈한 데이터를 네트워크에서 받아옴');
@@ -14,10 +16,12 @@ export default function Products() {
     return () => {
       console.log('🧹 깨끗하게 청소하는 일들을 합니다.');
     };
-  }, []);
+  }, [checked]);
 
   return (
     <>
+      <input if="checkbox" type='checkbox' value={checked} onChange={handleChange}/>
+      <label htmlFor='checkbox'>show only HOT sale</label>
       <ul>
         {products.map((product) => (
           <li key={product.id}>
