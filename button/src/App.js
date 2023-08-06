1e17ffd47b95a3a3ecd815b6a6c42ae83f2624e1
dyd0027 <dyd002700@gmail.com>
2023-08-06 14:35:10 +0900

    6.5tailwind

--- button/src/App.js
+++ button/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import StyledComponents from './StyledComponents';
+import TailWindComponents from './TailwindComponents';
 
 
 function App() {
   return (
     <>
      <StyledComponents/>
+     <TailWindComponents/>
     </>
   );
 }
---
+++ button/src/TailwindComponents.jsx
@@ -0,0 +1,8 @@
+export default function TailWindComponents(){
+    return(
+        <>
+            <h1 className="text-slate-600">안녕</h1>
+            <button className="bg-blue-200 rounded-xl px-2">버튼이요~</button>
+        </>
+    )
+}
