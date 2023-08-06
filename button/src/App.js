db86861893ce9c3d8876a91943affd1e7bdf7569
dyd0027 <dyd002700@gmail.com>
2023-08-06 14:13:38 +0900

    6.4Styled Components

---
+++ button/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import {styled,css} from 'styled-components';
+
+const Container = styled.div`
+  display:flex;
+`;
+
+const Button = styled.button`
+  backgroud : transparent;
+  border-radius : 3px;
+  border : 2px solid #3c5b69;
+  color: red;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  ${(props) => 
+    props.primary && 
+    css`
+      background: #009cd5;
+      color: white;
+    `
+  }
+`;
+
+function App() {
+  return (
+    <>
+      <Container>
+        <Button>노말버튼</Button>
+        <Button primary>특별버튼</Button>
+      </Container>
+    </>
+  );
+}
+
+export default App;
