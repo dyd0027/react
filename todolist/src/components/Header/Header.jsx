515507f9d6abbd7d4fb38c5429c5829b6bdfdc21
dyd0027 <dyd002700@gmail.com>
2023-08-07 21:42:33 +0900

    8.15스타일링하기

--- todolist/src/components/Header/Header.jsx
+++ todolist/src/components/Header/Header.jsx
@@ -1,13 +1,23 @@
-export default function Header({filters, filter,onFilterChange}){
-    return(
-        <header>
-            <ul>
-                {
-                    filters.map((f,idx)=>
-                        (<li key={idx}><button onClick={()=>onFilterChange(f)}>{f}</button></li>)
-                    )
-                }
-            </ul>
-        </header>
-    )
+import React from 'react';
+import styles from './Header.module.css';
+
+export default function Header({ filters, filter, onFilterChange }) {
+  return (
+    <header className={styles.header}>
+      <ul className={styles.filters}>
+        {filters.map((value, index) => (
+          <li key={index}>
+            <button
+              className={`${styles.filter} ${
+                filter === value && styles.selected
+              }`}
+              onClick={() => onFilterChange(value)}
+            >
+              {value}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </header>
+  );
 }
