0e4bde3c8ccd893946e01511d188f4529d543b80
dyd0027 <dyd002700@gmail.com>
2023-08-09 21:37:34 +0900

    아울렛 사용하기

--- router/src/App.js
+++ router/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import Videos from './pages/Videos';
+import NotFound from './pages/NotFound';
+import Root from './pages/Root';
 
 const router = createBrowserRouter([
   {
     path:'/',
-    element: <p>HOME</p>,
-    errorElement: <p>error</p>
-  },
-  {
-    path:'/aa',
-    element: <p>aa</p>
+    element: <Root/>,
+    errorElement: <NotFound/>,
+    children: [{index: true, element:<Home/>},
+    {path:'/videos',element:<Videos/>}]
   }
 ]);
 
 function App() {
   return (
-    <RouterProvider router = {router} >
-      APP
-    </RouterProvider>
+    <RouterProvider router = {router} />
   );
 }
 
